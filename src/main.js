bb01b33430bfc831239e8af0b8e35d203f7885b6
Arien de Groot <arien.degroot@incentro.com>
2025-04-24 13:53:42 +0200

    Add custom events

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const customEventButton = document.querySelector(".custom-event");
 
-setupCounter(document.querySelector('#counter'))
+customEventButton.addEventListener("click", () => {
+  console.log("custom event");
+  window.gtag("event", "custom_event", {
+    event_category: "engagement",
+    event_label: "button_click",
+    value: "2",
+  });
+});
