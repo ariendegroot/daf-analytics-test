2c47a3e5a81e9c5e90bdb9c27eac5a18dbc36048
Arien de Groot <arien.degroot@incentro.com>
2025-04-24 14:11:18 +0200

    Update events

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import "./style.css";
 const customEventButton = document.querySelector(".custom-event");
 
 customEventButton.addEventListener("click", () => {
-  console.log("custom event");
-  window.gtag("event", "download_request", {
-    event_category: "Download",
-    event_label: "Step Files Download",
+  console.log("custom event 2d");
+  window.gtag("event", "request_download", {
+    event_category: "download_request",
+    event_label: "2d files download",
+    value: "1",
+  });
+});
+
+const customEventButtonStep = document.querySelector(".custom-event-step");
+customEventButtonStep.addEventListener("click", () => {
+  console.log("custom event step");
+  window.gtag("event", "request_download", {
+    event_category: "download_request",
+    event_label: "Step files downloads",
     value: "2",
   });
 });
+
